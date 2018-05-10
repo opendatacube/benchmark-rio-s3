b01b70496f54765d609db019de748e2bead8a6fe
Kirill Kouzoubov <kirill888@gmail.com>
2018-05-10 14:02:25 +1000

    Split PReadRIO_bench into generic and benchmarking part
    Generic part is responsible for running processing threads and setting up
    rasterio.Env for each thread and opening files, it then delegates to callback to
    do the rest.
    
    Benchmarking part defines a callback that reads one block from the file while
    keeping track of timing.

--- benchmark_rio_s3/pread_rio.py
+++ benchmark_rio_s3/pread_rio.py
@@ -5,12 +5,50 @@ import threading
 import boto3
 from .parallel import ParallelStreamProc
 
-__all__ = ["PReadRIO_bench"]
+__all__ = ["PReadRIO", "PReadRIO_bench"]
 
 _thread_lcl = threading.local()
 
 
-def get_boto3_session(region_name='ap-southeast-2'):
+def ec2_metadata(timeout=0.1):
+    import requests
+    try:
+        with requests.get('http://169.254.169.254/latest/dynamic/instance-identity/document', timeout=timeout) as resp:
+            if resp.ok:
+                return resp.json()
+            return None
+    except IOError:
+        return None
+
+
+def ec2_current_region():
+    cfg = ec2_metadata()
+    if cfg is None:
+        return None
+    return cfg.get('region', None)
+
+
+def botocore_default_region():
+    import botocore.session
+    return botocore.session.get_session().get_config_variable('region')
+
+
+def auto_find_region():
+    region_name = ec2_current_region()
+
+    if region_name is None:
+        region_name = botocore_default_region()
+
+    if region_name is None:
+        raise ValueError('Region name is not supplied and default can not be found')
+
+    return region_name
+
+
+def get_boto3_session(region_name=None):
+    if region_name is None:
+        region_name = ec2_current_region()
+
     sessions = getattr(_thread_lcl, 'sessions', None)
     if sessions is None:
         sessions = {}
@@ -25,53 +63,100 @@ def get_boto3_session(region_name='ap-southeast-2'):
     return session
 
 
-class PReadRIO_bench(object):
+class PReadRIO(object):
     @staticmethod
-    def block_stream_proc(src_stream,
-                          block,
-                          dst,
-                          stats_out,
-                          gdal_opts=None,
-                          region_name=None,
-                          band=1):
+    def process_files(src_stream,
+                      on_file_cbk,
+                      gdal_opts=None,
+                      region_name=None,
+                      timer=None):
         session = get_boto3_session(region_name)
 
-        with rasterio.Env(session=session, **gdal_opts):
-            for idx, url in src_stream:
-                t0 = t_now()
-                dst_slice = dst[idx, :, :]
-
+        if timer is not None:
+            def proc(url, userdata):
+                t0 = timer()
                 with rasterio.open(url, 'r') as f:
-                    win = f.block_window(band, *block)
-                    t1 = t_now()
-                    f.read(band, window=win, out=dst_slice)
-                    t2 = t_now()
-                    chunk_size = f.block_size(band, *block)
+                    on_file_cbk(f, userdata, t0=t0)
+        else:
+            def proc(url, userdata):
+                with rasterio.open(url, 'r') as f:
+                    on_file_cbk(f, userdata)
 
-                stats_out[idx] = SimpleNamespace(t_open=t1-t0,
-                                                 t_total=t2-t0,
-                                                 t0=t0,
-                                                 chunk_size=chunk_size)
+        with rasterio.Env(session=session, **gdal_opts):
+            for userdata, url in src_stream:
+                proc(url, userdata)
 
     def __init__(self, nthreads,
-                 region_name='ap-southeast-2',
-                 use_ssl=True):
+                 region_name=None):
+        if region_name is None:
+            region_name = auto_find_region()  # Will throw on error
+
         self._nthreads = nthreads
         self._pstream = ParallelStreamProc(nthreads)
-        self._rdr_block = self._pstream.bind(PReadRIO_bench.block_stream_proc)
-        self._use_ssl = use_ssl  # At least for now we ignore this param
+        self._process_files = self._pstream.bind(PReadRIO.process_files)
         self._region_name = region_name
 
         self._gdal_opts = dict(VSI_CACHE=True,
                                CPL_VSIL_CURL_ALLOWED_EXTENSIONS='tif',
                                GDAL_DISABLE_READDIR_ON_OPEN=True)
 
-    def warmup(self):
+    def warmup(self, action=None):
+        """Mostly needed for benchmarking needs. Ensures that worker threads are
+        started and have S3 credentials pre-loaded.
+
+        If you need to setup some thread-local state you can supply action
+        callback that will be called once in every worker thread.
+        """
         def _warmup():
             session = get_boto3_session(region_name=self._region_name)
+            if action:
+                action()
             return session.get_credentials()
 
         return self._pstream.broadcast(_warmup)
+
+    def process_bunch(self, stream, cbk, timer=None):
+        """
+        stream: (userdata, url)...
+        cbk:
+           file_handle, userdata -> None (ignored)|
+           file_handle, userdata, t0 -> None (ignored) -- when timer is supplied
+
+        timer: None| ()-> TimeValue
+
+        Equivalent to this serial code, but with many concurrent threads and
+        with appropriate `rasterio.Env` wrapper for S3 access
+
+        ```
+        for userdata, url in stream:
+            with rasterio.open(url,'r') as f:
+               cbk(f, userdata)
+        ```
+
+        if timer is set:
+        ```
+        for userdata, url in stream:
+            t0 = timer()
+            with rasterio.open(url, 'r') as f:
+               cbk(f, userdata, t0=t0)
+        ```
+        """
+        self._process_files(stream, cbk,
+                            self._gdal_opts,
+                            region_name=self._region_name,
+                            timer=timer)
+
+
+class PReadRIO_bench(object):
+    def __init__(self, nthreads,
+                 region_name=None,
+                 use_ssl=True):
+        self._nthreads = nthreads
+        self._use_ssl = use_ssl  # At least for now we ignore this param
+        self._proc = PReadRIO(nthreads, region_name=region_name)
+
+    def warmup(self):
+        return self._proc.warmup()
 
     def read_blocks(self,
                     urls,
@@ -79,15 +164,24 @@ class PReadRIO_bench(object):
                     dst,
                     band=1):
         t0 = t_now()
-
         stats = [None for _ in urls]
 
-        self._rdr_block(enumerate(urls), block_idx, dst, stats,
-                        gdal_opts=self._gdal_opts,
-                        region_name=self._region_name,
-                        band=band)
+        def extract_block(f, idx, t0=0):
+            dst_slice = dst[idx, :, :]
+            win = f.block_window(band, *block_idx)
+            t1 = t_now()
+            f.read(band, window=win, out=dst_slice)
+            t2 = t_now()
+            chunk_size = f.block_size(band, *block_idx)
 
-        t_total = t_now()-t0
+            stats[idx] = SimpleNamespace(t_open=t1-t0,
+                                         t_total=t2-t0,
+                                         t0=t0,
+                                         chunk_size=chunk_size)
+
+        self._proc.process_bunch(enumerate(urls), extract_block, timer=t_now)
+
+        t_total = t_now() - t0
         params = SimpleNamespace(nthreads=self._nthreads,
                                  band=band,
                                  block_shape=dst.shape[1:],
