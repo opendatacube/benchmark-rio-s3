ef09e8810efd2de5cd573c9e7850e12bc2dfbe91
Kirill Kouzoubov <kirill888@gmail.com>
2018-05-22 11:55:57 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='benchmark-rio-s3',
+    version='0.1',
+    license='Apache License 2.0',
+    url='https://github.com/Kirill888/benchmark-rio-s3',
+    packages=find_packages(),
+    author='Kirill Kouzoubov',
+    author_email='kirill.kouzoubov@ga.gov.au',
+    description='Tools for benchmarking multi-threaded performance of Rasterio/GDAL when accessing files on S3',
+    python_requires='>=3.5',
+    install_requires=['numpy',
+                      'rasterio',
+                      'requests',
+                      'botocore',
+                      'boto3',
+                      'click',
+                      ],
+    tests_require=['pytest'],
+)
